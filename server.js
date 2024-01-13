225f320579c6b32f9ccd32d624b40f50b3dadf43
Tiago Martins Pinto <55080281+ptiagomp@users.noreply.github.com>
2024-01-13 19:07:57 +0200

    update

--- server.js
+++ server.js
@@ -164,5 +164,5 @@ function handleDisconnect(socket, userName) {
 }
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
