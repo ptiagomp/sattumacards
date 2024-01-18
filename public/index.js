988e9d8cb31745f0d720a79efaf7fc09a76702cd
Tiago Martins Pinto <55080281+ptiagomp@users.noreply.github.com>
2024-01-18 15:13:20 +0200

    burger menu and logo

--- public/index.js
+++ public/index.js
@@ -31,3 +31,19 @@ function navigate(url) {
         window.location.href = url;
     }, 2000); // Delay matches the animation duration
 }
+
+
+function toggleMenu() {
+    var menuContent = document.getElementById("menuContent");
+    menuContent.style.display = menuContent.style.display === "block" ? "none" : "block";
+}
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+    // Optional: Close the burger menu when a modal is opened
+    document.getElementById("menuContent").style.display = 'none';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
