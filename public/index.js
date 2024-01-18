f0a508f80c4070864ac0e61f0bb3bee54aee6d36
Tiago Martins Pinto <55080281+ptiagomp@users.noreply.github.com>
2024-01-18 19:04:59 +0200

    landing page updates

--- public/index.js
+++ public/index.js
@@ -3,27 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function bindEventListeners() {
-    // const joinGameButton = document.getElementById('joinGameBtn');
-    // joinGameButton.addEventListener('click', handleJoinGameClick);
 
     const createGameButton = document.getElementById('createGameBtn');
     createGameButton.addEventListener('click', () => navigate('gameA.html'));
 }
 
-// function handleJoinGameClick() {
-//     const gameId = document.getElementById('joinGameInput').value.trim();
-
-//     if (isValidGameId(gameId)) {
-//         navigate(`gameA.html?gameId=${encodeURIComponent(gameId)}`);
-//     } else {
-//         alert('Please introduce a valid Game ID!');
-//     }
-// }
-
-// function isValidGameId(gameId) {
-//     // Implement additional validation logic if necessary
-//     return gameId.length > 0;
-// }
 
 function navigate(url) {
     document.body.classList.add('fade-out');
