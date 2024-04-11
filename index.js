59bd7df0e32ac301065f077afdfb086d858aa301
Tiago Martins Pinto <55080281+ptiagomp@users.noreply.github.com>
2024-04-11 16:48:45 +0300

    responsive

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function closeModal(modalId) {
   document.getElementById(modalId).style.display = "none";
 }
 
-window.onclick = function(event) {
-  if (event.target.classList.contains('modal')) {
+window.onclick = function (event) {
+  if (event.target.classList.contains("modal")) {
     closeModal(event.target.id);
   }
 };
